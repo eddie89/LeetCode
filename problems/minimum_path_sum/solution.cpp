f95fc54c364f47957a1aa5d970052dfe20223f7b
eddie89 <tremmeldaniel@gmail.com>
2020-04-22 16:11:13 +0000

    Sync LeetCode submission - Minimum Path Sum (cpp)

---
+++ problems/minimum_path_sum/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int h = grid.size();
+        int w = grid[0].size();
+        vector<vector<int>> dp(h, vector<int> (w));
+        for(int row = 0; row < h; row++) {
+            for(int col = 0; col < w; col++) {
+                if(row == 0 && col == 0) {
+                    dp[row][col] = grid[row][col];
+                } else {
+                    dp[row][col] = grid[row][col] + min((row == 0 ? INT_MAX : dp[row - 1][col]), (col == 0 ? INT_MAX : dp[row][col - 1]));
+                }
+            }
+        }
+        return dp[h - 1][w - 1];
+    }
+};
